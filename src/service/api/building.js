47781b80037ca5337826b5e5df87d9c2c35aa1f0
hmtheboy154 <buingoc67@gmail.com>
2026-01-13 06:41:24 +0700

    DashboardScreen: give a small window to view some building info
    - Things like water, electricity, service and due day.
    - We use FontAwesome5 for the service icon.
    - Refactor the code a bit by putting the styles down below.
    
    Signed-off-by: hmtheboy154 <buingoc67@gmail.com>

--- src/service/api/building.js
+++ src/service/api/building.js
@@ -16,3 +16,11 @@ export function getBuildings(params = {}) {
 export function getBuildingById(id) {
   return unwrap(http.get(`/building/${id}`));
 }
+
+/**
+ * Lấy thông tin chi tiết (giá điện, nước...) của các tòa nhà mà tenant đang thuê
+ * Backend: GET /building/tenant/my-info
+ */
+export function getMyBuildingDetails() {
+  return unwrap(http.get("/building/tenant/my-info"));
+}
