f52aa1a1812681ecc1464a5d552c86746b7cba78
dat <datchthk22@gmail.com>
2026-01-21 05:17:16 +0700

    modified contract action

--- src/screens/dashboard/DashboardScreen.js
+++ src/screens/dashboard/DashboardScreen.js
@@ -394,7 +394,7 @@ const styles = StyleSheet.create({
         elevation: 2,
     },
     sectionTitle: {
-        fontSize: 16,
+        fontSize: 12,
         fontWeight: "700",
         color: "#6B7280",
         marginBottom: 4,
